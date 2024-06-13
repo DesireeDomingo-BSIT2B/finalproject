73a9822a5d84de2c8fce1487900f418474de9e26
DesireeDomingo-BSIT2B <124164402+DesireeDomingo-BSIT2B@users.noreply.github.com>
2024-06-13 12:12:05 +0800

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,12 +2,25 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import requests
+import os
 
 st.title("Grapevine Image Classification")
 
+MODEL_URL = "https://github.com/DesireeDomingo-BSIT2B/finalproject/raw/main/grapevinemodel.keras"
+
+def download_model(url, filename):
+    if not os.path.exists(filename):
+        with st.spinner("Downloading model..."):
+            response = requests.get(url)
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            st.success("Model downloaded successfully!")
+
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('https://github.com/DesireeDomingo-BSIT2B/finalproject/blob/main/grapevinemodel.keras')
+    download_model(MODEL_URL, 'model.keras')
+    model = tf.keras.models.load_model('model.keras')
     return model
 
 model = load_model()
